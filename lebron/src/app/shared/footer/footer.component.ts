2d32eefc4435d91653e991569bf17937692894cf
chehin <chehinjuan@gmail.com>
2022-11-12 20:53:27 -0300

    more footer

--- lebron/src/app/shared/footer/footer.component.ts
+++ lebron/src/app/shared/footer/footer.component.ts
@@ -34,5 +34,21 @@ export class FooterComponent implements OnInit {
    });
   }
 
+  goTwitter() {
+    window.location.href='https://twitter.com/' + this.configuraciones.twitter;
+}
+
+goFacebook() {
+  window.location.href='https://www.facebook.com/' + this.configuraciones.facebook;
+}
+
+goInstagram() {
+  window.location.href='https://www.instagram.com/'+ this.configuraciones.instagram;
+}
+
+goYouTube() {
+  window.location.href='https://www.youtube.com/' + this.configuraciones.youtube;
+}
+
 
 }

b21793f73ad58de95aea4e02666a673002faadbe
chehin <chehinjuan@gmail.com>
2022-11-18 18:10:44 -0300

    base for send email

--- backend/src/controllers/clientesController.ts
+++ backend/src/controllers/clientesController.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction, response } from 'express';
 import pool from '../database';
 const bcrypt = require('bcrypt');
+const nodemailer = require("nodemailer");
+import keys from '../keys';
 
 // const path = require('path');
 
@@ -14,21 +16,6 @@ public async listarRoles(req: Request, res: Response): Promise<void> {
 
      res.json(roles);
  }
-// ==================================================
-//        Lista personas desde cierto valor
-// ==================================================
-    public async list(req: Request, res: Response): Promise<void> {
-        var desde = req.query.desde || 0;
-        desde  = Number(desde);
-
-        pool.query(`call bsp_listar_personas('${desde}')`, function(err: any, result: any, fields: any){
-            if(err){
-                console.log("error", err);
-                return;
-            }
-            res.json(result);
-        })
-    }
 
 // ==================================================
 //        Obtiene una personas de la BD
@@ -59,54 +46,10 @@ public async getOne(req: Request, res: Response): Promise<any> {
 
 }
 
-
-// ==================================================
-//        Busqueda por nombre - apellido
-// ==================================================
-public async buscar(req: Request, res: Response): Promise<any> {
-    const busqueda = req.params.busqueda;
-    
-    pool.query(`call bsp_buscar_persona('${busqueda}')`, function(err: any, result: any, fields: any){
-        if(err){
-            console.log("error : ", err);
-            res.status(404).json({ text: "La personas no existe" });
-        }
-        
-        return res.json(result);
-    })
-}
-
-// ==================================================
-//        Actualiza una persona
-// ==================================================
-
-public async update(req: Request, res: Response): Promise<void> {
-    const { id } = req.params;
-    await pool.query('bsp_modifica_persona(?,?)', [req.body, id]);
-    res.json({ message: "La persona se actualizo" });
-}
-
-// ==================================================
-//        Da de baja una persona
-// ==================================================
-public async baja(req: Request, res: Response): Promise<void> {
-    const id = req.query.termino;
-    const result = await pool.query('CALL bsp_darbaja_persona(?)', [id]);
-    res.json({ message: "Persona dada de baja" });
-}
-
-    
-// ===========================================================================
-// =========================== CLIENTES ==========================================
-// ===========================================================================
-
-
 // ==================================================
 //        Inserta un cliente
 // ==================================================
 public async altaCliente(req: Request, res: Response) {
-
-    console.log("pasa bsp_alta_cliente req.body : ",req.body)
 
     var Email = req.body[0];
     var Password = req.body[1];
@@ -133,6 +76,9 @@ public async altaCliente(req: Request, res: Response) {
                 }                
             
                 if(result[0][0].Mensaje !== 'Ok'){
+
+                    // enviarMailConfirmacion(Email);
+
                     return res.json({
                         ok: false,
                         Mensaje: result[0][0].Mensaje
@@ -148,6 +94,42 @@ public async altaCliente(req: Request, res: Response) {
 
 }
 
+// ==================================================
+//   
+// ==================================================
+
+public async enviarMailConfirmacion(pEmail: string) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: keys.mail.host,
+    port: keys.mail.port,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: keys.mail.user, // generated ethereal user
+      pass: keys.mail.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <lebron@example.com>', // sender address
+    to: pEmail, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
 
 // ==================================================
 //   Activa un cliente (caso de ya existencia en la BD)
@@ -265,186 +247,6 @@ public async actualizaCliente(req: Request, res: Response) {
 
 }
 
- // ==================================================
-//   Lista los clientes inscriptos en un cierto plan
-// ==================================================
-
-public async listarClientesPlan(req: Request, res: Response): Promise<void> {
-
-     var IdPlan = req.params.IdPlan || 0;
-     IdPlan  = Number(IdPlan);
-
-     var desde = req.params.desde || 0;
-     desde  = Number(desde);
-
-     pool.query(`call bsp_listar_clientes_plan('${desde}','${IdPlan}')`, function(err: any, result: any, fields: any){
-        if(err){
-            console.log("error", err);
-            return;
-        }
-        res.json(result);
-    })
- }
-
-// ===========================================================================
-// =========================== PROFESIONALES ==========================================
-// ===========================================================================
-
-
-// ==================================================
-//        Inserta un profesional
-// ==================================================
-
-
-public async createProfesional(req: Request, res: Response) {
-
-    var IdTipoDocumento = req.body.IdTipoDocumento;
-    var Apellidos = req.body.Apellidos;
-    var Nombres = req.body.Nombres;
-    var Documento = req.body.Documento;
-    var Password = req.body.Password;
-    var Telefono = req.body.Telefono;
-    var Sexo = req.body.Sexo;
-    var Observaciones = req.body.Observaciones;
-    var FechaNac = req.body.FechaNac;
-    var Correo = req.body.Correo;
-    var Usuario = req.body.Usuario;
-    var Calle = req.body.Calle;
-    var Piso = req.body.Piso;
-    var Departamento = req.body.Departamento;
-    var Ciudad = req.body.Ciudad;
-    var Pais = req.body.Pais;
-    var Numero = req.body.Numero;    // 19
-    var IdRol = req.body.IdRol; 
-
-    pool.query(`call bsp_alta_profesional('${IdTipoDocumento}','${IdRol}','${Apellidos}','${Nombres}'
-    ,'${Documento}','${Password}','${Telefono}','${Sexo}','${Observaciones}','${FechaNac}','${Correo}','${Usuario}'
-    ,'${Calle}',${Piso},'${Departamento}','${Ciudad}','${Pais}',${Numero})`, function(err: any, result: any, fields: any){
-        if(err){
-            console.log("error", err);
-            return;
-        }
-
-        if(result[0][0].Mensaje !== 'Ok'){
-            return res.json({
-                ok: false,
-                Mensaje: result[0][0].Mensaje
-            });
-        }
-    
-        res.json({ Mensaje: 'Ok' });
-        
-    })   
-
-}
-
-// ==================================================
-//        Actualiza un profesional
-// ==================================================
-public async actualizaProfesional(req: Request, res: Response) {
-
-    var IdPersona = req.body.IdPersona;
-    var IdTipoDocumento = req.body.IdTipoDocumento;
-    var Apellidos = req.body.Apellidos;
-    var Nombres = req.body.Nombres;
-    var Documento = req.body.Documento;
-    // var Password = bcrypt.hashSync(req.body.Password,10);         // Encriptacion de una sola via
-    var Password = req.body.Password;
-    var Telefono = req.body.Telefono;
-    var Sexo = req.body.Sexo;
-    var Observaciones = req.body.Observaciones;
-    var IdRol = req.body.IdRol;
-    var FechaNac = req.body.FechaNac;
-    var Correo = req.body.Correo;
-    var Usuario = req.body.Usuario;
-    var Calle = req.body.Calle;
-    var Piso = req.body.Piso;
-    var Departamento = req.body.Departamento;
-    var Ciudad = req.body.Ciudad;
-    var Pais = req.body.Pais;
-    var Numero = req.body.Numero;    // 20
-    var Estado = req.body.Estado; 
-
-    pool.query(`call bsp_actualiza_profesional('${IdPersona}','${IdTipoDocumento}','${IdRol}','${Apellidos}','${Nombres}'
-    ,'${Documento}','${Password}','${Telefono}','${Sexo}','${Observaciones}',${FechaNac},'${Correo}','${Usuario}'
-    ,'${Calle}','${Piso}','${Departamento}','${Ciudad}','${Pais}','${Numero}','${Estado}')`, function(err: any, result: any, fields: any){
-        if(err){
-            console.log("error", err);
-            return;
-        }
-
-        if(result[0][0].Mensaje !== 'Ok'){
-            return res.json({
-                ok: false,
-                Mensaje: result[0][0].Mensaje
-            });
-        }
-    
-        res.json({ Mensaje: 'Ok' });
-        
-    })
-
-}
-
-// ==================================================
-//   Da de baja un profesional
-// ==================================================
-public async darBajaProfesional(req: Request, res: Response) {
-
-    var IdPersona = req.params.IdPersona;
-
-    pool.query(`call bsp_darbaja_profesional('${IdPersona}')`, function(err: any, result: any, fields: any){
-        if(err){
-            console.log("error", err);
-            return;
-        }
-
-        if(result[0][0].Mensaje !== 'Ok'){
-            return res.json({
-                ok: false,
-                mensaje: result.Mensaje
-            });
-        }
-        return res.json({ mensaje: 'Ok' });
-        
-    })
-
-    
-
-}
- // ==================================================
-//        Lista el personal del gimnasio desde cierto valor
-// ==================================================
-public async listarPersonal(req: Request, res: Response): Promise<void> {
-    var desde =  req.params.desde;
-    var incluyeBajas =  req.params.incluyeBajas;
-
-    pool.query(`call bsp_listar_personal('${desde}','${incluyeBajas}')`, function(err: any, result: any, fields: any){
-        if(err){
-            console.log("error", err);
-            return;
-        }
-        res.json(result);
-    })
- }
-
- // ==================================================
-//        Lista el personal del gimnasio desde cierto valor
-//       Si en pDesde viene '-1' entonces se listan todos los profesionales
-// ==================================================
-
-public async listarProfesionales(req: Request, res: Response): Promise<void> {
-
-    pool.query(`call bsp_listar_profesionales()`, function(err: any, result: any, fields: any){
-        if(err){
-            console.log("error", err);
-            return;
-        }
-        
-        return res.json(result);
-        
-    })
- }
 }
 
 
--- backend/src/keys.ts
+++ backend/src/keys.ts
@@ -7,6 +7,14 @@ export default {
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME
+    },
+
+    mail: {
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        secure: process.env.MAIL_SECURE,
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
     }
 
 }

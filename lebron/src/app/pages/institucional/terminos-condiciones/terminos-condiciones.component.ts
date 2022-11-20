7ba84a2bf92bd3289782697326480186230f1704
chehin <chehinjuan@gmail.com>
2022-11-20 08:52:36 -0300

    terminos y condiciones

---
+++ lebron/src/app/pages/institucional/terminos-condiciones/terminos-condiciones.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+
+@Component({
+  selector: 'app-terminos-condiciones',
+  templateUrl: './terminos-condiciones.component.html',
+  styles: []
+})
+export class TerminosCondicionesComponent implements OnInit {
+
+  constructor(
+  ) {
+   }
+
+  ngOnInit() {
+  }
+
+// ==================================================
+// 
+// ==================================================
+
+
+
+}

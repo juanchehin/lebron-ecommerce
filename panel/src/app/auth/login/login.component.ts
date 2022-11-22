fa55e00e5f456989ff124edd76ee89eaaad6bbc8
chehin <chehinjuan@gmail.com>
2022-11-21 21:24:09 -0300

    login in new template

---
+++ panel/src/app/auth/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { AuthService } from 'src/app/services/auth.service';
+
+declare const gapi:any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: [ './login.component.css' ]
+})
+export class LoginComponent implements OnInit {
+
+
+  constructor( private router: Router,              
+               public authService: AuthService ) { }
+
+  ngOnInit(): void {
+    this.authService.logout();
+  }
+
+
+
+// ==================================================
+//  Proceso de LOGUEO
+// ==================================================
+ingresar(forma: NgForm) {
+
+  if ( forma.invalid ) {
+    return;
+  }
+
+  const persona = new Array(
+    forma.value.email,
+    forma.value.password
+  );
+
+  // Llamada al servicio
+
+  this.authService.login(persona)
+      .subscribe((resp: any) => {
+
+        if ( resp === true) {
+          this.router.navigate(['/dashboard']);
+          return;
+        }
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Error de credenciales',
+          text: 'Error de credenciales',
+        });
+    },
+    ( error: any) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Ha ocurrido un error',
+          text: 'Contactese con el administrador',
+        });
+    }
+
+    );
+
+}
+
+
+}
---
+++ panel/src/app/guards/admin.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UsuarioService } from '../services/usuario.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor( private usuarioService: UsuarioService,
+               private router: Router ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): any {
+    
+
+      // return (this.usuarioService.role === 'ADMIN_ROLE') ? true : false;
+
+  }
+  
+}

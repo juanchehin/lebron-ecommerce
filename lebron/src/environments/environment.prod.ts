4230c26b24a23e026d6983c7a5e44dabdb300455
chehin <chehinjuan@gmail.com>
2023-01-24 11:11:51 -0300

    add variables in enviroment prod

--- lebron/src/environments/environment.prod.ts
+++ lebron/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  URL_SERVICIOS: 'http://localhost:3000/api',
+  ruta_img_productos: 'http://localhost:3000/uploads/images/productos/',
+  ruta_img_marcas: 'http://localhost:3000/uploads/images/marcas/',
+  ruta_img: 'http://localhost:3000/images/'
 };

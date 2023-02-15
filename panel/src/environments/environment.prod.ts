2ee227688b8630825dfab48b18844158fa356ea8
chehin <chehinjuan@gmail.com>
2023-02-15 12:16:45 -0300

    modify endpoints

--- panel/src/environments/environment.prod.ts
+++ panel/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 export const environment = {
   production: true,
   base_url: '',
-  URL_SERVICIOS: 'http://localhost:3000/api',
-  ruta_img_productos: 'http://localhost:3000/uploads/images/productos/',
-  ruta_img_marcas: 'http://localhost:3000/uploads/images/marcas/'
+  URL_SERVICIOS: 'https://lebron.chehin.online:3000/api',
+  ruta_img_productos: 'https://lebron.chehin.online:3000/uploads/images/productos/',
+  ruta_img_marcas: 'https://lebron.chehin.online:3000/uploads/images/marcas/'
 };

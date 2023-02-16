10fa13e798f763ee240150934d6371eed53cb257
chehin <chehinjuan@gmail.com>
2023-02-16 19:37:52 -0300

    modify enviroment in prod

--- lebron/src/environments/environment.prod.ts
+++ lebron/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  URL_SERVICIOS: 'http://localhost:3000/api',
-  ruta_img_productos: 'http://localhost:3000/uploads/images/productos/',
-  ruta_img_marcas: 'http://localhost:3000/uploads/images/marcas/',
-  ruta_img: 'http://localhost:3000/images/'
+  URL_SERVICIOS: 'https://lebron.chehin.online:3000/api',
+  ruta_img_productos: 'https://lebron.chehin.online:3000/uploads/images/productos/',
+  ruta_img_marcas: 'https://lebron.chehin.online:3000/uploads/images/marcas/',
+  ruta_img: 'https://lebron.chehin.online:3000/images/'
 };

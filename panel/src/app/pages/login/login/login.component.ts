625917939d4a33e51c2a6eac7004e286dd9bec57
chehin <chehinjuan@gmail.com>
2023-02-23 12:11:13 -0300

    reload to close + modify alerts and form

---
+++ panel/src/app/pages/login/login/login.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit, NgZone, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { AlertService } from 'src/app/services/alert.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: [ './login.component.css' ]
+})
+export class LoginComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor( 
+    private router: Router,              
+    public authService: AuthService,
+    private alertService: AlertService
+  )
+  { }
+
+  ngOnInit(): void {
+    this.authService.logout();
+
+    this.form = new FormGroup({      
+      email: new FormControl(null, Validators.required ),
+      password: new FormControl(null, Validators.required )
+    });
+  }
+
+// ==================================================
+//  Proceso de LOGUEO
+// ==================================================
+ingresar() {
+
+  if ( this.form.invalid ) {
+    return;
+  }
+
+  const persona = new Array(
+    this.form.value.email,
+    this.form.value.password
+  );
+
+  this.authService.login(persona)
+      .subscribe((resp: any) => {
+        
+        if ( resp === true) {
+          this.router.navigate(['/dashboard']);
+          return;
+        }
+
+        this.alertService.alertFailWithText('Error','Error de credenciales',false,300)
+
+    },
+    ( error: any) => {
+      this.alertService.alertFailWithText('Atencion','Ocurrio un error, contactese con el adminsitrador - ' + error,false,300)
+
+    }
+
+    );
+
+}
+
+// Recarga para resetear el sidebar en al dashboard
+ngOnDestroy(): void {
+  window.location.reload();
+}
+
+}

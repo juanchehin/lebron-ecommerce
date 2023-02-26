27b2e643cee4f9b30b89b5859dcbc11b892d5c6d
chehin <chehinjuan@gmail.com>
2023-02-26 09:53:35 -0300

    service util

---
+++ panel/src/app/services/util.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+
+  constructor() { }
+
+// ==================================================
+//    Formatea la fecha a yyyy-mm-dd
+// ==================================================
+
+formatDateNow(date: any) {
+    // tslint:disable-next-line: one-variable-per-declaration
+    let d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + (d.getDate()),
+    // tslint:disable-next-line: prefer-const
+    year = d.getFullYear();
+  
+    if (month.length < 2) { month = '0' + month; }
+    if (day.length < 2) { day = '0' + day; }
+  
+    return [year, month, day].join('-');
+  }
+
+
+}

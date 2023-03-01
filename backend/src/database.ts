520ed2e2755239707abb424539394a4be3fb4538
chehin <chehinjuan@gmail.com>
2023-03-01 10:00:19 -0300

    return to keys database

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
 
-var database = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-};
 
-const pool = mysql.createPool(database);
+const pool = mysql.createPool(
+    process.env.DB_HOST,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    process.env.DB_NAME
+);
 
 pool.getConnection((err: any, connection: { release: () => void; }) => {
     if (err) throw err; connection.release(); 

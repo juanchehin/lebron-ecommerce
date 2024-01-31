b2c8799a25ce8bdc25b1aff5d68ecbc428275db9
chehin <chehinjuan@gmail.com>
2024-01-31 20:14:51 -0300

    modify endpoint

--- lebron/src/environments/environment.prod.ts
+++ lebron/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  URL_SERVICIOS: 'https://lebron.chehin.online:3000/api',
-  ruta_img_productos: 'https://lebron.chehin.online:3000/uploads/images/productos/',
-  ruta_img_marcas: 'https://lebron.chehin.online:3000/uploads/images/marcas/',
-  ruta_img: 'https://lebron.chehin.online:3000/images/'
+  URL_SERVICIOS: 'https://lebron.chehininformatica.shop:3003/api',
+  ruta_img_productos: 'https://lebron.chehininformatica.shop:3003/uploads/images/productos/',
+  ruta_img_marcas: 'https://lebron.chehininformatica.shop:3003/uploads/images/marcas/',
+  ruta_img: 'https://lebron.chehininformatica.shop:3003/images/'
 };
